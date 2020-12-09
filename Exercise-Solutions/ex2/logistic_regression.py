8f9f1374e1e8b44271267b1bbe0153b5db782037
Desmond <desmondtan4561@gmail.com>
2020-12-09 20:40:59 +0800

    updated ex2

--- Exercise-Solutions/ex2/logistic_regression.py
+++ Exercise-Solutions/ex2/logistic_regression.py
@@ -6,35 +6,112 @@ def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
 
-def getDerivativeMat(x, y, theta):
+def getGrad(theta, x, y):
     m, n = np.shape(x)
-    arr = [0] * n
-    for i in range(m):
-        for j in range(n):
-            arr[j] += (sigmoid(np.dot(theta, x[i])) - y[i]) * x[i][j]
+    theta = theta.reshape((n, 1))
+    y = y.reshape((m, 1))
 
-    return np.squeeze(np.asarray(arr))
+    grad = (x.transpose()).dot(sigmoid(x.dot(theta)) - y) / m
+    return grad.flatten()
 
 
 def costFunction(theta, x, y):
+    """
+    Compute cost and gradient for logistic regression
+    J = COSTFUNCTION(theta, X, y) computes the cost of using theta as the
+    parameter for logistic regression and the gradient of the cost
+    w.r.t. to the parameters.
 
-    m = len(y)
-    j = 0
-    theta = np.zeros(np.size(theta))
+    :param theta:
+    :param x:
+    :param y:
+    :return:
+    """
+    m, n = np.shape(x)
+    theta = theta.reshape((n, 1))
+    y = y.reshape((m, 1))
 
-    for i in range(m):
-        j += (-y[i] * np.log(sigmoid(np.dot(theta, x[i])))
-              - (1 - y[i]) * np.log(1 - sigmoid(np.dot(theta, x[i]))))
-    j /= m
+    first_part = np.log(sigmoid(x.dot(theta)))
+    second_part = np.log(1 - sigmoid(x.dot(theta)))
+    first_part = first_part.reshape((m, 1))
+    second_part = second_part.reshape((m, 1))
+    j = -(np.sum(y * first_part + (1 - y) * second_part) / m)
 
-    grad = getDerivativeMat(x, y, theta) / m
-    # grad = np.dot(x.transpose(), (sigmoid(np.dot(x, theta)) - y)) / m
-
-    return j, grad
+    return j
 
 
-def fmin(initial_theta, x, y):
-    result = op.fmin_tnc(func=costFunction,
-                         x0=initial_theta,
-                         args=(x, y))
-    return result
+def fmin_tnc(initial_theta, costfun, gradfun, args):
+    result = op.minimize(fun=costfun, x0=initial_theta, args=args, method='TNC', jac=gradfun,
+                         options={'maxiter': 400})
+    final_theta = result.x
+    cost = result.fun
+    return final_theta, cost
+
+
+def predict(theta, x):
+    """
+    Predict whether the label is 0 or 1 using learned logistic regression parameters theta
+    p = PREDICT(theta, X) computes the predictions for X using a
+    threshold at 0.5 (i.e., if sigmoid(theta'*x) >= 0.5, predict 1)
+
+    :param theta:
+    :param x:
+    :return:
+    """
+    probs = sigmoid(x.dot(theta))
+    return (probs > 0.5).astype(int)
+
+
+def mapFeature(x1, x2):
+    """
+    MAPFEATURE(X1, X2) maps the two input features to quadratic features used in the regularization exercise.
+
+    Returns a new feature array with more features, comprising of
+    X1, X2, X1.^2, X2.^2, X1*X2, X1*X2.^2, etc..
+
+    Inputs X1, X2 must be the same size
+
+    :param x1: vector of n dimension
+    :param x2: vector of n dimension
+    :return: matrix of (n * 28)
+    """
+    degree = 6
+    n = np.shape(x1)[0]
+    out = np.ones((n, 1))
+
+    for i in range(1, degree + 1):
+        for j in range(i + 1):
+            new = (x1 ** (i - j)) * (x2 ** j)
+            new = new.reshape((n, 1))
+            out = np.append(out, new, axis=1)
+
+    return out
+
+
+def costFunctionReg(theta, x, y, lam):
+    """
+    Compute cost and gradient for logistic regression with regularization
+    J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
+    theta as the parameter for regularized logistic regression and the
+    gradient of the cost w.r.t. to the parameters.
+
+    :param theta:
+    :param x:
+    :param y:
+    :param lam:
+    :return:
+    """
+    m, n = np.shape(x)
+    theta = theta.reshape((n, 1))
+
+    j = costFunction(theta, x, y) + (lam / (2 * m)) * np.sum(theta[1:] ** 2)
+
+    return j
+
+
+def getGradReg(theta, x, y, lam):
+    m, n = np.shape(x)
+    theta = theta.reshape((n, 1))
+    grad = getGrad(theta, x, y).reshape((n, 1)) + (lam / m) * theta
+    return grad.flatten()
+
--- Exercise-Solutions/ex2/test.py
+++ Exercise-Solutions/ex2/test.py
@@ -1,6 +1,7 @@
 from logistic_regression import *
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import svm
 import unittest
 
 
@@ -14,7 +15,7 @@ def getDataFromFile(filename, dtype=float):
     with open(filename, 'r') as f:
         for line in f:
             data = line.strip().split(',')
-            x_val, y_val = list(map(dtype, data[:-1])), dtype(data[-1])
+            x_val, y_val = list(map(dtype, data[:-1])), int(data[-1])
             x.append(x_val)
             y.append(y_val)
     return np.squeeze(np.asarray(x)), np.squeeze(np.asarray(y))
@@ -33,13 +34,48 @@ def plotStudentData(x, y):
     plt.show()
 
 
+def plotDecisionBoundary(x, y, final_theta):
+    xx = np.linspace(np.min(x), np.max(x))
+    grad = -final_theta[1] / final_theta[2]
+    yy = grad * xx - final_theta[0] / final_theta[2]
+
+    plt.plot(xx, yy, 'k-')
+    plotStudentData(x, y)
+
+
+def plotMicrochipsData(x, y):
+    accepted = x[y[:] == 1, :]
+    rejected = x[y[:] == 0, :]
+
+    plt.plot(accepted[:, 0], accepted[:, 1], 'k+')
+    plt.plot(rejected[:, 0], rejected[:, 1], 'ko')
+
+    plt.xlabel('Microchips Test 1')
+    plt.ylabel('Microchips Test 2')
+    plt.legend(['Accepted', 'Rejected'])
+    plt.show()
+
+
+def plotBoundary2(theta, x, y):
+    u = np.linspace(-1, 1.5, 50)
+    v = np.linspace(-1, 1.5, 50)
+    z = np.zeros(shape=(len(u), len(v)))
+    for i in range(len(u)):
+        for j in range(len(v)):
+            z[i, j] = (mapFeature(np.array([u[i]]), np.array([v[j]])).dot(theta))
+
+    z = z.T
+    plt.contour(u, v, z)
+    plotMicrochipsData(x, y)
+
+
 ########################################################################################################################
 # Test Code
 ########################################################################################################################
 
 class TestEx2(unittest.TestCase):
     def setUp(self) -> None:
-        self.x1, self.y1 = getDataFromFile('ex2data1.txt', dtype=np.float128)
+        self.x1, self.y1 = getDataFromFile('ex2data1.txt')
         self.x2, self.y2 = getDataFromFile('ex2data2.txt')
 
     def getVisualisation(self):
@@ -59,25 +95,102 @@ class TestEx2(unittest.TestCase):
         x = np.c_[np.ones(m), self.x1]
         initial_theta = np.zeros(n + 1)
 
-        cost, grad = costFunction(initial_theta, x, self.y1)
+        cost = costFunction(initial_theta, x, self.y1)
+        grad = getGrad(initial_theta, x, self.y1)
         self.assertAlmostEqual(0.693, cost, places=3)
         np.testing.assert_array_almost_equal(
-            np.asarray([-0.1000,  -12.0092, -11.2628]),
+            np.asarray([-0.1000, -12.0092, -11.2628]),
             grad, decimal=4)
 
-    def testfmin(self):
+    def testfmin_tnc(self):
         m, n = np.shape(self.x1)
         x = np.c_[np.ones(m), self.x1]
-        initial_theta = np.zeros((n + 1), dtype=int)
-        print(fmin(initial_theta, x, self.y1))
+        initial_theta = np.zeros((n + 1))
+        final_theta, cost = fmin_tnc(initial_theta, costfun=costFunction, gradfun=getGrad, args=(x, self.y1))
+
+        # print('Cost at theta found by fmin_tnc:', cost)
+        # print('Theta computed from truncated newton algorithm:', final_theta)
+        self.assertAlmostEqual(0.203, cost, places=3)
+
+    def getDecisionBoundary(self):
+        m, n = np.shape(self.x1)
+        x = np.c_[np.ones(m), self.x1]
+        initial_theta = np.zeros((n + 1))
+        final_theta, cost = fmin_tnc(initial_theta, costfun=costFunction, gradfun=getGrad, args=(x, self.y1))
+
+        plotDecisionBoundary(self.x1, self.y1, final_theta)
+
+    def testAccuracy(self):
+        m, n = np.shape(self.x1)
+        x = np.c_[np.ones(m), self.x1]
+        initial_theta = np.zeros((n + 1))
+        final_theta, cost = fmin_tnc(initial_theta, costfun=costFunction, gradfun=getGrad, args=(x, self.y1))
+
+        # Predict probability for a student with score 45 on exam 1 and score 85 on exam 2
+        prob = sigmoid(np.matmul(np.asarray([1, 45, 85]), final_theta))
+        # print('For a student with scores 45 and 85, we predict an admission probability of ', prob)
+        self.assertAlmostEqual(0.776, prob, places=3)
+
+        # Compute accuracy on our training set
+        p = predict(final_theta, x)
+        accuracy = np.mean(np.equal(p, self.y1).astype(int)) * 100
+        print('Train Accuracy: ', accuracy)
+
+    def getVisualization2(self):
+        plotMicrochipsData(self.x2, self.y2)
+
+    def testMapFeature(self):
+        x1 = self.x2[:, 0]
+        x2 = self.x2[:, 1]
+        n = np.shape(x1)[0]
+        mapped = mapFeature(x1, x2)
+        self.assertEqual((n, 28), np.shape(mapped))
+
+    def testCostFunctionReg(self):
+        x1 = self.x2[:, 0]
+        x2 = self.x2[:, 1]
+        mapped = mapFeature(x1, x2)
+        m, n = np.shape(mapped)
+
+        lam = 1
+        initial_theta = np.zeros((n, 1))
+        cost = costFunctionReg(initial_theta, mapped, self.y2, lam)
+        grad = getGradReg(initial_theta, mapped, self.y2, lam)
+
+        self.assertAlmostEqual(0.693, cost, places=3)
+        self.assertEqual((28,), np.shape(grad))
+
+    def testAccuracy2(self):
+        x1 = self.x2[:, 0]
+        x2 = self.x2[:, 1]
+        mapped = mapFeature(x1, x2)
+        m, n = np.shape(mapped)
+
+        lam = 1
+        initial_theta = np.zeros(n)
+        final_theta, cost = fmin_tnc(initial_theta, costfun=costFunctionReg, gradfun=getGradReg,
+                                     args=(mapped, self.y2, lam))
+        print(final_theta, cost)
+
+    def getDecisionBoundary2(self):
+        x1 = self.x2[:, 0]
+        x2 = self.x2[:, 1]
+        mapped = mapFeature(x1, x2)
+        m, n = np.shape(mapped)
+
+        lam = 1
+        initial_theta = np.zeros(n)
+        final_theta, cost = fmin_tnc(initial_theta, costfun=costFunctionReg, gradfun=getGradReg,
+                                     args=(mapped, self.y2, lam))
+        plotBoundary2(final_theta, self.x2, self.y2)
 
 
 def main():
     test = TestEx2()
     test.setUp()
-    test.getVisualisation()
+    test.getDecisionBoundary2()
 
 
 if __name__ == '__main__':
-    unittest.main()
-    # main()
+    # unittest.main()
+    main()
